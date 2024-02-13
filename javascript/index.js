500955594f7366cc91f0d6441ec0c85e9ca67c78
Pinguina-Git <151978028+Pinguina-Git@users.noreply.github.com>
2024-02-13 17:43:59 +1300

    Added tokyo as city and moment timezone

--- javascript/index.js
+++ javascript/index.js
@@ -5,6 +5,14 @@ setInterval(() => {
   let dateFormat = moment().format("MMMM Do YYYY");
   dateElement.innerHTML = dateFormat;
 
-  let timeFormat = moment().format("h:mm:ss:SSSS [<small>]A[</small>] ");
+  let timeFormat = moment().format("h:mm:ss [<small>]A[</small>] ");
   timeElement.innerHTML = timeFormat;
-}, 100);
+
+  // let dateFormat = moment().format("MMMM Do YYYY");
+  let tokyoTimeZone = moment().tz("Asia/Tokyo");
+  let tokyoTime = document.querySelector(".tokyo .time");
+  let tokyoDate = document.querySelector(".tokyo .date");
+
+  tokyoDate.innerHTML = tokyoTimeZone.format("MMMM Do YYYY");
+  tokyoTime.innerHTML = tokyoTimeZone.format("h:mm:ss [<small>]A[</small>] ");
+}, 1000);
