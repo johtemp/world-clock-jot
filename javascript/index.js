8c43659751ac72b505dff2aec2c65700893b28b7
Pinguina-Git <151978028+Pinguina-Git@users.noreply.github.com>
2024-02-16 12:02:48 +1300

    Added current location

--- javascript/index.js
+++ javascript/index.js
@@ -1,55 +1,54 @@
-//   let timeElement = document.querySelector(".time");
-//   let dateElement = document.querySelector(".date");
+function updateTime() {
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let lostAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-//   let dateFormat = moment().format("MMMM Do YYYY");
-//   dateElement.innerHTML = dateFormat;
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
+    lostAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-//   let timeFormat = moment().format("h:mm:ss [<small>]A[</small>] ");
-//   timeElement.innerHTML = timeFormat;
-// }, 1000);
+  // Paris
+  let parisElement = document.querySelector("#paris");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
 
-// Tokyo
+    parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
 
-//1. Create a variable to Select Element
-//2. Add a listener to change when selecting a city
-//3. Create a function on changing time chnge when selecting with a parameter
-//4. Create a variable for timezone
-//5. Create variable for city name
-//6. Create variable for city Time
-//7. Create a variable for the div with formatting
-
-let tokyoTimeZone = moment().tz("Asia/Tokyo");
-let tokyoTime = document.querySelector(".tokyo .time");
-let tokyoDate = document.querySelector(".tokyo .date");
-
-tokyoDate.innerHTML = tokyoTimeZone.format("MMMM Do YYYY");
-tokyoTime.innerHTML = tokyoTimeZone.format("h:mm:ss [<small>]A[</small>] ");
-
-function updateCityTime(event) {
+function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-  let cityElement = document.querySelector("#city-container");
-
-  cityElement.innerHTML = `
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
   <div class="city">
     <div>
       <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-          </div>
-          <div class="time">${cityTime.format(
-            "h:mm:ss [<small>]A[</small>]"
-          )}</div>
-        </div>`;
-
-  //   clearInterval(currentInterval);
-  //   currentInterval = setInterval(
-  //     () =>
-  //       (cityElement.querySelector(".time").innerHTML = moment()
-  //         .tz(cityTimeZone)
-  //         .format("h:mm:ss [<small>]A[</small>]")),
-  //     1000
-  //   );
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
 }
-let citiesSelect = document.querySelector("#cities");
-citiesSelect.addEventListener("change", updateCityTime);
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
